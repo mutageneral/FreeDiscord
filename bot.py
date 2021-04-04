084b0946ed7ced9bd6310651b30623b313d7ba9e
SKBotNL <svenj08@protonmail.com>
2021-04-04 20:48:43 +0200

    Removed cool

--- bot.py
+++ bot.py
@@ -61,19 +61,6 @@ async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
 
-@bot.group()
-async def cool(ctx):
-    """Says if a user is cool.
-    In reality this just checks if a subcommand is being invoked.
-    """
-    if ctx.invoked_subcommand is None:
-        await ctx.send('No, {0.subcommand_passed} is not cool'.format(ctx))
-
-@cool.command(name='bot')
-async def _bot(ctx):
-    """Is the bot cool?"""
-    await ctx.send('Yes, the bot is cool.')
-
 @bot.command()
 async def ping(ctx):
     '''
