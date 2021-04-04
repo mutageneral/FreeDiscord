27a69fe7b30596a553a319f09f1e04ff8083d6a3
SKBotNL <svenj08@protonmail.com>
2021-04-04 21:02:21 +0200

    Fixed the avatar command and added userinfo

--- bot.py
+++ bot.py
@@ -46,9 +46,11 @@ async def choose(ctx, *choices: str):
         await ctx.send(random.choice(choices))
 
 @bot.command()
-async def avatar(ctx, member: discord.Member):
+async def avatar(ctx, *, user: discord.Member = None):
     """Get a link to somebody's avatar"""
-    await ctx.send(member.avatar_url)
+    if user is None:
+        user = ctx.author
+    await ctx.send(user.avatar_url)
 
 @bot.command()
 async def repeat(ctx, times: int, content='repeating...'):
@@ -72,6 +74,26 @@ async def ping(ctx):
     # Send it to the user
     await ctx.send(latency)
 
-
+@bot.command()
+async def userinfo(ctx, *, user: discord.Member = None):
+    if user is None:
+        user = ctx.author      
+    date_format = "%a, %d %b %Y %I:%M %p"
+    embed = discord.Embed(color=0xdfa3ff, description=user.mention)
+    embed.set_author(name=str(user), icon_url=user.avatar_url)
+    embed.set_thumbnail(url=user.avatar_url)
+    embed.add_field(name="Joined", value=user.joined_at.strftime(date_format))
+    members = sorted(ctx.guild.members, key=lambda m: m.joined_at)
+    embed.add_field(name="Join position", value=str(members.index(user)+1))
+    embed.add_field(name="Registered", value=user.created_at.strftime(date_format))
+    if len(user.roles) > 1:
+        role_string = ' '.join([r.mention for r in user.roles][1:])
+        embed.add_field(name="Roles [{}]".format(len(user.roles)-1), value=role_string, inline=False)
+    perm_string = ', '.join([str(p[0]).replace("_", " ").title() for p in user.guild_permissions if p[1]])
+    embed.add_field(name="Guild permissions", value=perm_string, inline=False)
+    embed.set_footer(text='ID: ' + str(user.id))
+    return await ctx.send(embed=embed)
+    if isinstance(ctx.channel, discord.DMChannel):
+        return
 
 bot.run('ODI4Mjg3NTMxMzI1NDU2Mzk0.YGnY4A.ppqNJ9hizvv1UrxrNZz-8Hj-voE')
