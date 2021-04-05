88cd015f576f17b4169ff86a17fe5064c238c83b
SKBotNL <svenj08@protonmail.com>
2021-04-05 17:12:44 +0200

    Errorhandling, add kick, fix purge, rm custom help

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ intents.members = True
 ownerID = "Put your ID here"
 
 bot = commands.Bot(command_prefix='@', description=description, intents=intents)
-bot.remove_command('help')
+# bot.remove_command('help')
 
 @bot.event
 async def on_ready():
@@ -24,6 +24,13 @@ async def on_ready():
 bot.load_extension("cogs.general")
 bot.load_extension("cogs.utils")
 bot.load_extension("cogs.moderation")
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error,commands.MissingPermissions):
+        await ctx.send("You dont have permission to do that")
+    if isinstance(error,commands.MissingRequiredArgument):
+        await ctx.send("Your command is missing an argument")
 
 @bot.command()
 async def add(ctx, left: int, right: int):
--- cogs/general.py
+++ cogs/general.py
@@ -4,14 +4,14 @@ class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot   
 
-    @commands.command(invoke_without_command=True)
-    async def help(self, ctx):
-        em = discord.Embed(title = "Help", description = "Use [prefix]help <command> for extended information on a command.")
-        em.add_field(name = "General", value = "help")
-        em.add_field(name = "Moderation", value = "purge")
-        em.add_field(name = "Utils", value = "avatar, joined, ping, userinfo")
+    # @commands.command(invoke_without_command=True)
+    # async def help(self, ctx):
+        # em = discord.Embed(title = "Help", description = "Use [prefix]help <command> for extended information on a command.")
+        # em.add_field(name = "General", value = "help")
+        # em.add_field(name = "Moderation", value = "purge")
+        # em.add_field(name = "Utils", value = "avatar, joined, ping, userinfo")
 
-        await ctx.send(embed = em)
+        # await ctx.send(embed = em)
 
 def setup(bot):
     bot.add_cog(General(bot))
