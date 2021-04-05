a57a3babea577f2a1abe026d24a83c6fad4b8df0
its just lag <80059602+ItsJustLag@users.noreply.github.com>
2021-04-05 15:50:03 -0400

    Update settings.py

--- cogs/settings.py
+++ cogs/settings.py
@@ -4,14 +4,5 @@ class Settings(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-@commands.command()
-async def botstatus(ctx, statusMsg):
-    """Sets the status of the bot. Requires double quotes."""
-    if ownerID == str(ctx.message.author.id):
-        await bot.change_presence(activity=discord.Game(name=statusMsg))
-        await ctx.send("Status successfully changed!")
-    else:
-        await ctx.send("'Owner Only.'")
-
 def setup(bot):
     bot.add_cog(Settings(bot))
