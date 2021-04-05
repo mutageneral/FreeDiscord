3f04391cd530d0c7ae1ee9215468575187de29a0
its just lag <80059602+ItsJustLag@users.noreply.github.com>
2021-04-05 16:13:25 -0400

    added setStatus back

--- cogs/settings.py
+++ cogs/settings.py
@@ -1,8 +1,22 @@
 import discord
 from discord.ext import commands
+
+ownerID = "YOUR Discord userID here"
+
+
 class Settings(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def setStatus(self, ctx, status):
+        """Sets the status of the bot. Owner only."""
+        if str(ctx.message.author.id) == ownerID:
+            await self.bot.change_presence(activity=discord.Game(name=status))
+            await ctx.send("Bot status successfully changed to " + status + "!")
+        else:
+            await ctx.send("listen here bud the command says 'Owner Only' for a reason")
+
 
 def setup(bot):
     bot.add_cog(Settings(bot))
