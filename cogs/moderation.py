7b18342833a60e8330971da141ed0e34dc373c8d
its just lag <80059602+ItsJustLag@users.noreply.github.com>
2021-04-06 11:15:10 -0400

    POOOOOG FIXED MUTE COMMAND fiesurgtfywietyvofufyt

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -5,18 +5,18 @@ from discord.utils import get
 import asyncio
 
 
+def timeconvertion(period):  # Time convertion
+    if period[-1].isalpha():
+        to_convert = ''.join(filter(str.isdigit, period))
+        convertion = {"s": 1, "m": 60, "h": 3600, "d": 86400}
+        timeconverted = int(to_convert) * convertion[period[-1]]
+        return int(timeconverted)
+    else:
+        return 0
+
 class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    def timeconvertion(period):  # Time convertion
-        if period[-1].isalpha():
-            to_convert = ''.join(filter(str.isdigit, period))
-            convertion = {"s": 1, "m": 60, "h": 3600, "d": 86400}
-            timeconverted = int(to_convert) * convertion[period[-1]]
-            return int(timeconverted)
-        else:
-            return 0
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
@@ -51,12 +51,16 @@ class Moderation(commands.Cog):
     @commands.command()  # Takes 1s 1m 1h 1d
     @commands.has_permissions(manage_messages=True)
     async def mute(self, ctx, user: discord.Member, mutetime):
-        """Mute a member"""
-        role = discord.utils.get(user.guild.roles, name="muted")
-        await user.add_roles(role)
-        await ctx.send("User muted for " + "``{}``".format(str(mutetime)) + " seconds.")
-        await asyncio.sleep(timeconvertion(mutetime))
-        await user.remove_roles(role)
+        if timeconvertion(mutetime) != 0:
+            role = discord.utils.get(user.guild.roles, name="muted")
+            await user.add_roles(role)
+            await ctx.send("User muted for " + "`{}`".format(str(mutetime)) + ".")
+            await asyncio.sleep(timeconvertion(mutetime))
+            await user.remove_roles(role)
+        elif timeconvertion(time) == 0:
+            await ctx.send("Time format is not right.")
+        else:
+            print("Something went wrong")
 
 
 def setup(bot):
