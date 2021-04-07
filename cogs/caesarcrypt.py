c0a2e33639c4394fdcfc2d847cf234a3cd18f374
odysseus443 <zaeruess@protonmail.com>
2021-04-08 00:43:54 +0800

    Try now.

--- cogs/caesarcrypt.py
+++ cogs/caesarcrypt.py
@@ -5,20 +5,19 @@ class Caesarcrypt(commands.Cog):
         self.bot = bot
 
     @commands.command(description="Play with caesarcrypt. @bot rounds(numbers) message")
-    async def encrypt(ctx, rounds: int, *message: str):
+    async def encrypt(self, ctx, rounds: int, *message: str):
         """Encrypt a message."""
         encrypt = ""
-        try:
-            int(rounds)
-            for char in message:
-                if not char.isalpha():
-                    encrypt = encrypt + char
-                elif char.isupper():
-                    # for uppercase Z
-                    encrypt = encrypt + chr((ord(char) + rounds - 65) % 26 + 365)
-                else:
-                    # for lowercase z
-                    encrypt = encrypt + chr((ord(char) + rounds - 97) % 26 + 97)
+        message = str(message)
+        for char in message:
+            if not char.isalpha():
+                encrypt = encrypt + char
+            elif char.isupper():
+                # for uppercase Z
+                encrypt = encrypt + chr((ord(char) + rounds - 65) % 26 + 365)
+            else:
+                # for lowercase z
+                encrypt = encrypt + chr((ord(char) + rounds - 97) % 26 + 97)
             em = discord.Embed(title = 'Your encrypted message is: {}'.format(encrypt))
             await ctx.send(embed = em)
         except ValueError:
@@ -27,20 +26,19 @@ class Caesarcrypt(commands.Cog):
 
 
     @commands.command(description="Decrypt with caesarcrypt. @bot rounds(numbers) message")
-    async def decrypt(ctx, rounds: int, *message: str):
+    async def decrypt(self, ctx, rounds: int, *message: str):
         """Decrypt a message."""
         decrypt = ""
-        try:
-            int(rounds)
-            for char in message:
-                if not char.isalpha():
-                    decrypt = decrypt + char
-                elif char.isupper():
-                    # for uppercase Z
-                    decrypt = decrypt + chr((ord(char) - rounds - 65) % 26 + 65)
-                else:
-                    # for lowercase z
-                    decrypt = decrypt + chr((ord(char) - rounds - 97) % 26 + 97)
+        message = str(message)
+        for char in message:
+            if not char.isalpha():
+                decrypt = decrypt + char
+            elif char.isupper():
+                # for uppercase Z
+                decrypt = decrypt + chr((ord(char) - rounds - 65) % 26 + 65)
+            else:
+                # for lowercase z
+                decrypt = decrypt + chr((ord(char) - rounds - 97) % 26 + 97)
 
             em = discord.Embed(title = 'Your decrypted message is: {}'.format(decrypt))
             await ctx.send(embed = em)
