783cd1c4a5666622bc01d73af36a2d03076a1b88
its just lag <80059602+ItsJustLag@users.noreply.github.com>
2021-04-06 20:32:02 -0400

    Add files via upload

---
+++ cogs/botstatuschange.py
@@ -0,0 +1,70 @@
+import discord
+from discord.ext import commands
+import os
+import sys
+import time
+import asyncio
+sys.path.append(os.path.realpath('.'))
+from config import *
+
+class Settings(commands.Cog):
+        
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def botstatusrepeat(self, ctx):
+        if str(ctx.message.author.id) == ownerID:
+            await ctx.send("Status loop initiated.")
+
+            while True:
+                await self.bot.change_presence(activity=discord.Game("Activate Windows"))
+                await asyncio.sleep(5)
+                await self.bot.change_presence(activity=discord.Game("Fixing Bugs"))
+                await asyncio.sleep(5)
+                await self.bot.change_presence(activity=discord.Game("Made by the FreeTechnologies team"))
+                await asyncio.sleep(5) 
+                await self.bot.change_presence(activity=discord.Game("What am I doing with my life?"))
+                await asyncio.sleep(5) 
+                await self.bot.change_presence(activity=discord.Game("never gonna give you up, never gonna let you down"))
+                await asyncio.sleep(5) 
+                await self.bot.change_presence(activity=discord.Game("THE MINIMODS ARE COMING!!!"))            
+                await asyncio.sleep(5) 
+                await self.bot.change_presence(activity=discord.Game("no one cares bro"))
+                await asyncio.sleep(5) 
+                await self.bot.change_presence(activity=discord.Game("hey there buddy chum pal friend buddy pal chum bud friend fella brother amigo pal"))
+                await asyncio.sleep(5)
+                await self.bot.change_presence(activity=discord.Game("made by two weebs and one 3 year old"))
+                await asyncio.sleep(5)
+                await self.bot.change_presence(activity=discord.Game("ya like jazz?"))
+                await asyncio.sleep(5)
+                await self.bot.change_presence(activity=discord.Game("we got a, number 1 victory royal, yeah fortnite we 'bout to get down"))
+                await asyncio.sleep(5)
+                await self.bot.change_presence(activity=discord.Game("userDespacito"))
+                await asyncio.sleep(5)
+                await self.bot.change_presence(activity=discord.Game("this python script is waaaaay too long. meh, who cares"))
+                await asyncio.sleep(5)
+                await self.bot.change_presence(activity=discord.Game("SUS MONGUS"))
+                await asyncio.sleep(5)
+                await self.bot.change_presence(activity=discord.Game("hey guys just wanna remind you this is a christian minecraft server"))
+                await asyncio.sleep(5)
+                await self.bot.change_presence(activity=discord.Game("IT'S TIME TO STOP, OKAY?"))
+                await asyncio.sleep(5)
+                await self.bot.change_presence(activity=discord.Game("stop it. get some help."))
+                await asyncio.sleep(5)
+                await self.bot.change_presence(activity=discord.Game("somebody once told me the world was macaroni"))
+                await asyncio.sleep(5)
+                await self.bot.change_presence(activity=discord.Game("cringe"))
+                await asyncio.sleep(5)
+                await self.bot.change_presence(activity=discord.Game("Among Us"))
+                await asyncio.sleep(5)
+                await self.bot.change_presence(activity=discord.Game("fartnut"))
+                await asyncio.sleep(5)
+                await self.bot.change_presence(activity=discord.Game("creeper? aw man"))
+                await asyncio.sleep(5)
+                await self.bot.change_presence(activity=discord.Game("FreeDiscord is the best open source discord bot"))
+
+def setup(bot):
+    bot.add_cog(Settings(bot))
+
+
