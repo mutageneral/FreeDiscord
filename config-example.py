7e2f75919083ef057be0c1c9f27178f182ae5f54
SKBotNL <68018116+SKBotNL@users.noreply.github.com>
2021-04-07 08:09:37 +0200

    Rm example since @recallwhoiam got setup working

--- config-example.py
+++
@@ -1,4 +0,0 @@
-bot_token = 'Your Bot Token Here'
-prefix = '@' # this is the default prefix, you can change it to something else if you want
-ownerID = 'Your ID Here'
-virustotal_api = 'Your VirusTotal API Key Here'
