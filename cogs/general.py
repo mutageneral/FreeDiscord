bf648447fd421a6ab8f084829f9a02fd85140637
recall 3 <81481152+recallwhoiam@users.noreply.github.com>
2021-04-07 21:03:54 -0500

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -3,7 +3,15 @@ from discord.ext import commands
 class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot   
+    
+    @commands.command()
+    async def about(self, ctx):
+        em = discord.Embed(title = "About FreeDiscord", description = "This bot is based off of/is the FreeDiscord bot made by SKbotNL, ItsJustLag, recallwhoiam, Quirinus, and antistalker.")
+        em.add_field(name = "Project URL", value = "https://github.com/FreeTechnologies/FreeDiscord/")
+        em.add_field(name = "Support Server", value = "https://discord.gg/VyNxSt55gj")
 
+        await ctx.send(embed = em)
+        
     # @commands.command(invoke_without_command=True)
     # async def help(self, ctx):
         # em = discord.Embed(title = "Help", description = "Use [prefix]help <command> for extended information on a command.")
