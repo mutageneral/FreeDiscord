2a9f2a5e0e8a0eee52d37130195e8739df1abfda
SKBotNL <68018116+SKBotNL@users.noreply.github.com>
2021-04-09 19:12:40 +0200

    fix bad_words is not defined

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ bot.load_extension("cogs.vt_scan")
 
 @bot.event 
 async def on_message(msg):
-    for word in bad_words:
+    for word in config.bad_words:
         if word in msg.content.lower():
             await msg.delete()
             await msg.channel.send("Please don't use that word", delete_after=5.0)
