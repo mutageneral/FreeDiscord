f5ecb3d5e08ccd97cd7a60fa9f253a69fb79a895
recall 3 <81481152+recallwhoiam@users.noreply.github.com>
2021-04-08 20:25:13 -0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,8 +4,9 @@ import random
 import time
 import requests
 import json
-from config import *
+import config
 import base64
+import psutil
 
 description = '''List of all the commands
 -----------------------------------------
@@ -18,17 +19,20 @@ Support Server: https://discord.gg/VyNxSt55gj
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix=prefix, description=description, intents=intents)
+bot = commands.Bot(command_prefix=config.prefix, description=description, intents=intents)
 bot.remove_command('help')
 
 
 @bot.event
 async def on_ready():
     # What gets printed in the terminal when the bot is succesfully logged in
+    servers = list(bot.guilds)
+    serverNumber = len(servers)
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+    print(serverNumber)
 
 bot.load_extension("cogs.general")
 bot.load_extension("cogs.utils")
@@ -37,59 +41,84 @@ bot.load_extension("cogs.settings")
 bot.load_extension("cogs.caesarcrypt")
 bot.load_extension("cogs.help")
 bot.load_extension("cogs.update")
+bot.load_extension("cogs.admin")
 
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.MissingPermissions):
-        em = discord.Embed(title = "You do not have permission to do that.")
-        await ctx.send(embed = em)
-    elif isinstance(error, commands.MissingRequiredArgument):
-        em = discord.Embed(title = "Your command is missing an argument.")
-        await ctx.send(embed = em)
-    elif isinstance(error, commands.CommandNotFound):
-        em = discord.Embed(title = "Command not found")
-        await ctx.send(embed = em)
-    else:
-        em = discord.Embed(title = "An internal error occurred.")
-        await ctx.send(embed = em)
+#@bot.event
+#async def on_command_error(ctx, error):
+#    if isinstance(error, commands.MissingPermissions):
+#        em = discord.Embed(title = "You do not have permission to do that.")
+#        await ctx.send(embed = em)
+#    elif isinstance(error, commands.MissingRequiredArgument):
+#        em = discord.Embed(title = "Your command is missing an argument.")
+#        await ctx.send(embed = em)
+#    elif isinstance(error, commands.CommandNotFound):
+#        em = discord.Embed(title = "Command not found")
+#        await ctx.send(embed = em)
+#    else:
+#        em = discord.Embed(title = "An internal error occurred.", description = "Here are the details of the error: " + str(error))
+#        await ctx.send(embed = em)
+
+@bot.command(description='Shows information about bot instance.')
+async def about(ctx):
+    em = discord.Embed(title = "About FreeDiscord", description = "This bot is based off of/is the FreeDiscord bot made by SKBotNL, ItsJustLag, recallwhoiam, Quirinus, and antistalker#8888.")
+    em.add_field(name = "Project URL", value = "https://github.com/FreeTechnologies/FreeDiscord/")
+    em.add_field(name = "Support server", value = "https://discord.gg/VyNxSt55gj")
+    em.add_field(name = "Bot invite link", value = "https://discord.com/oauth2/authorize?client_id=829158610965495848&permissions=8&scope=bot")
+    servers = list(bot.guilds)
+    serverNumber = len(servers)
+    em.add_field(name = "Number of servers this instance is in", value = serverNumber)
+    cpuUsage = psutil.cpu_percent()
+    em.add_field(name = "CPU usage of host", value = cpuUsage)
+    em.add_field(name = "Ping in milliseconds", value = bot.latency*1000)
+    await ctx.send(embed = em)
 
 @bot.command()
 async def add(ctx, left: int, right: int):
     """Adds two numbers together."""
-    em = discord.Embed(title = left + right)
-    await ctx.send(embed = em)
-
+    if config.bot_lockdown_status == 'no_lockdown':
+        em = discord.Embed(title = left + right)
+        await ctx.send(embed = em)
+    elif config.bot_lockdown_status == "lockdown_activated":
+        em = discord.Embed(title = "This bot is locked down", description = "<@!" + config.ownerID + "> has locked down this bot globally.")
+        await ctx.send(embed = em)
 
 @bot.command()
 async def roll(ctx, dice: str):
     """Rolls a dice in N-N format."""
-    try:
-        rolls, limit = map(int, dice.split('-'))
-    except Exception:
-        em = discord.Embed(title = "Format has to be in `Number-Number`")
+    if config.bot_lockdown_status == 'no_lockdown':
+        try:
+            rolls, limit = map(int, dice.split('-'))
+        except Exception:
+            em = discord.Embed(title = "Format has to be in `Number-Number`")
+            await ctx.send(embed = em)
+            return
+
+        result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+        em = discord.Embed(title = result)
         await ctx.send(embed = em)
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    em = discord.Embed(title = result)
-    await ctx.send(embed = em)
-
+    elif config.bot_lockdown_status == "lockdown_activated":
+        em = discord.Embed(title = "This bot is locked down", description = "<@!" + config.ownerID + "> has locked down this bot globally.")
+        await ctx.send(embed = em)
 
 @bot.command(description='For when you wanna settle the score some other way')
 async def choose(ctx, *choices: str):
     """Chooses between multiple choices."""
-    if "@everyone" in choices:
-        em = discord.Embed(title = "Nice try, sadly that won't work here.")
-        await ctx.send(embed = em)
-    else:
-        if "@here" in choices:
+    if config.bot_lockdown_status == 'no_lockdown':
+        if "@everyone" in choices:
             em = discord.Embed(title = "Nice try, sadly that won't work here.")
             await ctx.send(embed = em)
         else:
-            em = discord.Embed(title = random.choice(choices))
-            await ctx.send(embed = em)
+            if "@here" in choices:
+                em = discord.Embed(title = "Nice try, sadly that won't work here.")
+                await ctx.send(embed = em)
+            else:
+                em = discord.Embed(title = random.choice(choices))
+                await ctx.send(embed = em)
+    elif config.bot_lockdown_status == "lockdown_activated":
+        em = discord.Embed(title = "This bot is locked down", description = "<@!" + config.ownerID + "> has locked down this bot globally.")
+        await ctx.send(embed = em)
 
-apikey = virustotal_api
+apikey = config.virustotal_api
 
 def vt_json_parsing(detections):
     detections = json.dumps(detections)
@@ -107,21 +136,29 @@ def vt_json_parsing(detections):
 @bot.command(description='Testing, "@bot hash"')
 async def vt_hash(ctx, hash: str):
     """VirusTotal Integration"""
-    header = {'x-apikey': '{}'.format(apikey)}
-    vturl = "https://www.virustotal.com/api/v3/files/{}".format(hash)
-    response = requests.get(vturl, headers = header).json()
-    em = discord.Embed(title = "Detections: {}".format(vt_json_parsing(response)))
-    await ctx.send(embed = em)
+    if config.bot_lockdown_status == 'no_lockdown':
+        header = {'x-apikey': '{}'.format(apikey)}
+        vturl = "https://www.virustotal.com/api/v3/files/{}".format(hash)
+        response = requests.get(vturl, headers = header).json()
+        em = discord.Embed(title = "Detections: {}".format(vt_json_parsing(response)))
+        await ctx.send(embed = em)
+    elif config.bot_lockdown_status == "lockdown_activated":
+        em = discord.Embed(title = "This bot is locked down", description = "<@!" + config.ownerID + "> has locked down this bot globally.")
+        await ctx.send(embed = em)
 
 @bot.command(description='Testing, "@bot hash"')
 async def scan_url(ctx, url: str):
     #Need to import base64 module to work
-    header = {'x-apikey': '{}'.format(apikey)}
-    encode = base64.b64encode(url.encode("utf-8"))
-    url_in_base64 = str(encode, "utf-8").replace("=", "")
-    vturl = "https://www.virustotal.com/api/v3/urls/{}".format(url_in_base64)
-    response = requests.get(vturl, headers = header).json()
-    em = discord.Embed(title = "Detections: {}".format(vt_json_parsing(response)))
-    await ctx.send(embed = em)
+    if config.bot_lockdown_status == 'no_lockdown':
+        header = {'x-apikey': '{}'.format(apikey)}
+        encode = base64.b64encode(url.encode("utf-8"))
+        url_in_base64 = str(encode, "utf-8").replace("=", "")
+        vturl = "https://www.virustotal.com/api/v3/urls/{}".format(url_in_base64)
+        response = requests.get(vturl, headers = header).json()
+        em = discord.Embed(title = "Detections: {}".format(vt_json_parsing(response)))
+        await ctx.send(embed = em)
+    elif config.bot_lockdown_status == "lockdown_activated":
+        em = discord.Embed(title = "This bot is locked down", description = "<@!" + config.ownerID + "> has locked down this bot globally.")
+        await ctx.send(embed = em)
 
-bot.run(bot_token)
+bot.run(config.bot_token)
