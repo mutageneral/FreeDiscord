e0b45e084b3f9a2b7cedde9a013cad6655780e46
odysseus443 <81030083+odysseus443@users.noreply.github.com>
2021-04-18 16:52:05 +0800

    This might be fixed the VT api for unknown url.

--- cogs/vt_scan.py
+++ cogs/vt_scan.py
@@ -1,7 +1,7 @@
 import discord, time
 from discord.ext import commands
 import config
-import json, base64, requests
+import json, base64, requests, asyncio
 
 apikey = config.virustotal_api
 
@@ -15,6 +15,8 @@ def vt_json_parsing(detections):
         if 'malicious' in str(m) and any(d.isdigit() for d in m):
             detections = m
             break
+        else:
+            return -1
     detections = "".join(filter(str.isdigit, m))
     return int(detections)
 
@@ -40,12 +42,23 @@ class VT(commands.Cog):
         #Need to import base64 module to work
         if config.bot_lockdown_status == 'no_lockdown':
             header = {'x-apikey': '{}'.format(apikey)}
+            data = {'url': url}
+            vturl = "https://www.virustotal.com/api/v3/urls"
+            requests.post(vturl, data = data, headers = header)
+            em = discord.Embed(title = "Please wait for 30 seconds")
+            await ctx.send(embed = em)
+            await asyncio.sleep(30)
             encode = base64.b64encode(url.encode("utf-8"))
             url_in_base64 = str(encode, "utf-8").replace("=", "")
             vturl = "https://www.virustotal.com/api/v3/urls/{}".format(url_in_base64)
             response = requests.get(vturl, headers = header).json()
-            em = discord.Embed(title = "Detections: {}".format(vt_json_parsing(response)))
-            await ctx.send(embed = em)
+            parsed = vt_json_parsing(response)
+            if parsed == -1:
+                em = discord.Embed(title = "Something went wrong.")
+                await ctx.send(embed = em)
+            else:
+                em = discord.Embed(title = "Detections: {}".format(parsed))
+                await ctx.send(embed = em)
         elif config.bot_lockdown_status == "lockdown_activated":
             em = discord.Embed(title = "This bot is locked down", description = "<@!" + config.ownerID + "> has locked down this bot globally.")
             await ctx.send(embed = em)
