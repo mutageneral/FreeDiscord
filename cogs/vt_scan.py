4ace80b41bca3671bbad9f5bc129f4d5b6259a91
odysseus443 <81030083+odysseus443@users.noreply.github.com>
2021-04-20 21:33:49 +0800

    Remove the dangerous link after user ask to scan.

--- cogs/vt_scan.py
+++ cogs/vt_scan.py
@@ -41,6 +41,7 @@ class VT(commands.Cog):
     @commands.command()
     async def scan_url(self, ctx, url: str):
         #Need to import base64 module to work
+        await ctx.message.delete()
         if config.bot_lockdown_status == 'no_lockdown':
             header = {'x-apikey': '{}'.format(apikey)}
             data = {'url': url}
