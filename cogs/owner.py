3b84c11a7cf12b709e1088319adab3df3f0c4105
SKBotNL <68018116+SKBotNL@users.noreply.github.com>
2021-04-20 08:25:53 +0200

    rename to owner

--- cogs/owner.py
+++ cogs/owner.py
@@ -6,7 +6,7 @@ import importlib
 import subprocess
 import os
 
-class Admin(commands.Cog):
+class Owner(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -80,4 +80,4 @@ class Admin(commands.Cog):
             await ctx.send(embed = em)
 
 def setup(bot):
-    bot.add_cog(Admin(bot))
+    bot.add_cog(Owner(bot))
